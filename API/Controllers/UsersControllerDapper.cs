718a8269bb4ea2b01aa0b8737933f1b312f4bc81
Sanby9837 <Sanbylai@gmail.com>
2024-04-01 00:47:06 +0800

    20240401 MVC+API Dapper get success

---
+++ API/Controllers/UsersControllerDapper.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Server.Models.Entity;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersControllerDapper : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public UsersControllerDapper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var users = connection.Query<Users>("SELECT * FROM Users");
+                return Ok(users);
+            }
+        }
+
+    }
+}
